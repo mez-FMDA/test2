a7605c84d0322596655fcc98cb2932004d31cb22
jfernandes-fmar <joel.fernandes@elexon.co.uk>
2025-06-19 17:47:38 +0100

    Update swagger-initializer.js

--- docs/Market_Facilitator/FMAR_Design/dist/swagger-initializer.js
+++ docs/Market_Facilitator/FMAR_Design/dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    src: "specs.json",
+    url: "specs.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
