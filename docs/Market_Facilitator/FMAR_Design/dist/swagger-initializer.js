20b0c1c9e72e7e1c896c8199e8667c0042b8e596
jfernandes-fmar <joel.fernandes@elexon.co.uk>
2025-06-20 09:27:13 +0100

    Update swagger-initializer.js

--- docs/Market_Facilitator/FMAR_Design/dist/swagger-initializer.js
+++ docs/Market_Facilitator/FMAR_Design/dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://github.com/mez-FMDA/MF.github.io/blob/main/docs/Market_Facilitator/FMAR_Design/dist/specs.json",
+    url: "https://raw.githubusercontent.com/mez-FMDA/MF.github.io/main/docs/Market_Facilitator/FMAR_Design/dist/specs.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
